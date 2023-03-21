a57a35a42b4a4462d8cc9437ab38a2958adc399a
kaiouken21 <ramadhanregan334@gmail.com>
2023-03-21 14:41:27 +0700

    first commit

---
+++ suku.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int sukuN, hitung;
+    cout <<"Masukan Suku yang inngin anda: " << endl;
+    cin >> sukuN;
+
+    hitung = sukuN*2;
+     cout << "Hasilnya adalah " << hitung << endl;
+     return 0;
+}
